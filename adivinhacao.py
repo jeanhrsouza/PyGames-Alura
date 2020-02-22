38e7684a0fa84a64ef59dc79a0f0c68db92fde93
Jean Souza <jeanhrsouza@hotmail.com>
2020-02-22 17:41:07 -0300

    Python 3 parte 1: O laco com while

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,26 +3,35 @@ print("Bem vindo no jogo de Adivinhação!")
 print("***********************************")
 
 numero_secreto = 42 #declaração de variáveis
-
-chute_str = input("Digite o seu número: ")
-# a variável que está no input é uma string. é necessário trocar para int
-#para trocar para int basta fazer um casting --> chute=int(chute_str)
-print("Você digitou ", chute_str)
-
-chute = int(chute_str)
+total_de_tentativas = 3
+rodada = 1
 
 
-acertou = chute == numero_secreto
-chute_maior = chute > numero_secreto
-chute_menor = chute < numero_secreto
+while( rodada <= total_de_tentativas  ):
+    print("Tentativa", rodada, "de ", total_de_tentativas)
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou ", chute_str)
+    chute = int(chute_str)
 
-#Como fazer if. é necessário utilizar os dois pontos :
-if(acertou):
-    print("Você acertou")
-else:
-    if(chute_maior):
-        print("Você errou! O seu chute foi maior do que o número secreto.")
-    elif(chute_menor):
-        print("Você errou! O seu chute foi menor do que o número secreto.")
+    acertou = chute == numero_secreto
+    chute_maior = chute > numero_secreto
+    chute_menor = chute < numero_secreto
 
-print("Fim do jogo")
+    #Como fazer if. é necessário utilizar os dois pontos :
+    if(acertou):
+        print("Você acertou")
+    else:
+        if(chute_maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(chute_menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+
+    rodada = rodada + 1
+
+
+
+
+
+
+    print("Fim do jogo")
+
