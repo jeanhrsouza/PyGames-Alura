31f604a56cb0f8a3f822f7504f27f1e3f2c360c3
Jean Souza <jeanhrsouza@hotmail.com>
2020-03-02 16:43:46 -0300

    Iterando de maneira diferente

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,14 +4,18 @@ print("***********************************")
 
 numero_secreto = 42 #declaração de variáveis
 total_de_tentativas = 3
-rodada = 1
 
 
-while( rodada <= total_de_tentativas  ):
-    print("Tentativa", rodada, "de ", total_de_tentativas)
-    chute_str = input("Digite o seu número: ")
+
+for rodada in range(1, total_de_tentativas + 1)  :
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou ", chute_str)
     chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue #continua com a próxima iteração
 
     acertou = chute == numero_secreto
     chute_maior = chute > numero_secreto
@@ -20,18 +24,13 @@ while( rodada <= total_de_tentativas  ):
     #Como fazer if. é necessário utilizar os dois pontos :
     if(acertou):
         print("Você acertou")
+        break
     else:
         if(chute_maior):
             print("Você errou! O seu chute foi maior do que o número secreto.")
         elif(chute_menor):
             print("Você errou! O seu chute foi menor do que o número secreto.")
 
-    rodada = rodada + 1
 
-
-
-
-
-
-    print("Fim do jogo")
+print("Fim do jogo")
 
