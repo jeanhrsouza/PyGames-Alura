4c8c27df805a5730641bc0e1843a92273331f298
Jean Souza <jeanhrsouza@hotmail.com>
2020-03-23 19:39:58 -0300

    Encontrando as letras no jogo da forca

--- forca.py
+++ forca.py
@@ -10,6 +10,14 @@ def jogar():
 
     #enquanto (True)
     while(not enforcou and not acertou):
+
+        chute = input("Qual letra?")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
         print("jogando....")
 
     print("Fim do jogo")
